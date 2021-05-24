a75cb191588161534101652a3c17bbb2d1156b88
JoKing <joebryant614@gmail.com>
2021-05-23 23:35:19 -0700

    everything works but delete

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,20 +1,27 @@
 const router = require('express').Router();
-const {validateNote , createNewNote} = require('../../lib/notes')
-const {notes} = require('../../db/db.json')
+const {createNewNote, filterOutById } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+const { nanoid } = require('nanoid');
+const fs = require('fs');
 
-router.get('/api/notes', (req, res) => {
+
+router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 
-router.post('/api/notes', (req, res) => {
-    if (!validateNote(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
-    }
+router.post('/notes', (req, res) => {
+
+    req.body.id = nanoid(10);
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
 
+router.delete('/notes/:id', (req, res) => {
+
+    const result = filterOutById(req.params.id, notes);
+    res.json(result)
+});
 
 module.exports = router;
